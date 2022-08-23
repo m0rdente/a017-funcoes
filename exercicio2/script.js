8fc99e1cc55d5c862b0064ce9400849c76fbcd78
Priscila Mordente <priscilamordente@gmail.com>
2022-08-23 14:41:57 -0300

    feito exercicio a017

---
+++ exercicio2/script.js
@@ -0,0 +1,36 @@
+//A
+
+/* let soma = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(soma(8, 6)) */
+
+//B
+
+/* let numeros = (num1, num2) => {  
+return num1 >= num2
+}
+
+console.log(numeros(8,6)) */
+
+//C
+
+/* let num = (numero) => {
+if(numero %2 === 0){
+    return "é par"
+}else{
+    return "é impar"
+}
+}
+
+console.log(num(50)) */
+
+//D
+
+/* let string = prompt("digite algo aqui")
+
+function frase(){
+console.log("tamanho da string:", string.length, "maiuscula:", string.toUpperCase())
+}
+frase(string) */
